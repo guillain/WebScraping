db3b3287a4afb0d0806a0e4fdbc5a1a788cc413e
guillain <guillain@gmail.com>
2018-07-06 00:01:37 +0200

    add data fct (data_mapping & load)

--- classes/report.py
+++ classes/report.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 class Report:
     def __init__(self, default):
@@ -22,6 +23,11 @@ class Report:
                 report.volume
             ))
 
+    def load(self, filename):
+        with open(filename, 'rb') as csvfile:
+            data = csv.reader(csvfile, delimiter=',')
+        return data
+
     def save_header(self, filename):
         if self.check_exec:
             with open(filename, 'w') as f:
@@ -35,7 +41,6 @@ class Report:
                 ))
 
     def save_content(self, filename, report):
-        print(filename, report)
         if self.check_exec:
             with open(filename, 'a') as f:
                 f.write('{},{},{},{},{}\n'.format(
@@ -54,3 +59,16 @@ class Report:
                 if not os.path.isfile(filename):
                     self.save_header(filename)
                 self.save_content(filename, report)
+
+    def data_mapping(self, markets, reports):
+        for report in reports:
+            if report.name not in markets:
+                markets[report.name] = []
+            markets[report.name].append(report)
+
+        """for key, value in markets.items():
+            print(key, value)
+            for v in value:
+                print("  ", v)
+        """
+        return markets
